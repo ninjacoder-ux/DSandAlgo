ec4f0e7055e93c7a307f7fd2f800c65693f2f144
ninjacoder-ux <hitesh_sikka2002@yahoo.co.in>
2023-07-19 17:30:00 -0400

    ISA&I

---
+++ Sorting/insertionsort.js
@@ -0,0 +1,16 @@
+function insertionsort(arr){
+    for(let i =1; i<arr.length; i++){
+        let currentVal = arr[i];
+        let j;
+        for(j= i-1; j >= 0 && arr[j] > currentVal; j--){
+            arr[j+1] = arr[j];
+        }
+        
+        arr[j+1] = currentVal;
+        
+    }
+    return arr;
+}
+
+insertionsort([2,1,9,76,4])
+
